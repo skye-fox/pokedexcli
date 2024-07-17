0ad364e56fadc0e84c8af3cad924f21ac54405a3
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-17 17:38:47 -0500

    Add function commandHelp

---
+++ cmd_help.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func commandHelp() error {
+	fmt.Println("\nUsage:")
+	fmt.Println()
+	for _, cmd := range getCommands() {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
+	}
+	fmt.Println()
+	return nil
+}
