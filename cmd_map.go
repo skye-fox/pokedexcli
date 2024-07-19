98d1fba95c0e6e7b5566ed0aa35d586dab593be6
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-19 00:28:59 -0500

    Add functions commandMap & commandMapB

---
+++ cmd_map.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func commandMap(cfg *config) error {
+	locationResp, err := cfg.pokeapiClient.LocationList(cfg.next)
+	if err != nil {
+		return err
+	}
+
+	cfg.next = locationResp.Next
+	cfg.previous = locationResp.Previous
+
+	for _, loc := range locationResp.Results {
+		fmt.Printf("\n%v", loc.Name)
+	}
+	fmt.Println()
+	fmt.Println()
+	return nil
+}
+
+func commandMapB(cfg *config) error {
+	if cfg.previous == nil {
+		fmt.Println()
+		return fmt.Errorf("Already on first page\n")
+	}
+
+	locationResp, err := cfg.pokeapiClient.LocationList(cfg.previous)
+	if err != nil {
+		return err
+	}
+
+	cfg.next = locationResp.Next
+	cfg.previous = locationResp.Previous
+
+	for _, loc := range locationResp.Results {
+		fmt.Printf("\n%v", loc.Name)
+	}
+	fmt.Println()
+	fmt.Println()
+	return nil
+}
