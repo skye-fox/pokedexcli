30e074b0274cc0660d8a155fc886c622aa836303
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-20 15:27:54 -0500

    Modify client struct to store a cache & NewClient() to accept an interval to pass into the cache

--- internal/pokeapi/client.go
+++ internal/pokeapi/client.go
@@ -3,14 +3,18 @@ package pokeapi
 import (
 	"net/http"
 	"time"
+
+	"github.com/skye-fox/pokedexcli/internal/pokecache"
 )
 
 type Client struct {
+	cache      pokecache.Cache
 	httpClient http.Client
 }
 
-func NewClient(timeout time.Duration) Client {
+func NewClient(interval time.Duration, timeout time.Duration) Client {
 	return Client{
+		cache: pokecache.NewCache(interval),
 		httpClient: http.Client{
 			Timeout: timeout,
 		},
