59b547c0e5428914a00ba7596b3d7941ddb37bce
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-21 01:26:57 -0500

    Update commandHelp function to accept second parameter

--- cmd_help.go
+++ cmd_help.go
@@ -2,7 +2,7 @@ package main
 
 import "fmt"
 
-func commandHelp(cfg *config) error {
+func commandHelp(cfg *config, input []string) error {
 	fmt.Println("\nUsage:")
 	fmt.Println()
 	for _, cmd := range getCommands() {
