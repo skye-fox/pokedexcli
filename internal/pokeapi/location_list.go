f5e59f1c558656c72a9181cdb4a6db13ea3fc750
skye-fox <105951568+skye-fox@users.noreply.github.com>
2024-07-21 01:21:42 -0500

    Change JsonLocations to jsonLocations

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -6,43 +6,43 @@ import (
 	"net/http"
 )
 
-func (c *Client) LocationList(pageURL *string) (JsonLocations, error) {
-	url := baseURL + "location/"
+func (c *Client) LocationList(pageURL *string) (jsonLocations, error) {
+	url := baseURL + "location-area/"
 	if pageURL != nil {
 		url = *pageURL
 	}
 
 	if cacheData, ok := c.cache.Get(url); ok {
-		locationResp := JsonLocations{}
+		locationResp := jsonLocations{}
 		err := json.Unmarshal(cacheData, &locationResp)
 		if err != nil {
-			return JsonLocations{}, err
+			return jsonLocations{}, err
 		}
 		return locationResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		return JsonLocations{}, err
+		return jsonLocations{}, err
 	}
 
 	resp, err := c.httpClient.Do(req)
 	if err != nil {
-		return JsonLocations{}, err
+		return jsonLocations{}, err
 	}
 	defer resp.Body.Close()
 
 	data, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return JsonLocations{}, err
+		return jsonLocations{}, err
 	}
 
 	c.cache.Add(url, data)
 
-	locationResp := JsonLocations{}
+	locationResp := jsonLocations{}
 	err = json.Unmarshal(data, &locationResp)
 	if err != nil {
-		return JsonLocations{}, err
+		return jsonLocations{}, err
 	}
 
 	return locationResp, nil
